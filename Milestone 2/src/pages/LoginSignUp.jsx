9ee2a3436a74d1f5e786f5e3ccedb163d66f6e7f
unknown <viniciusfrj@gmail.com>
2023-12-17 20:23:46 -0300

    i TRIED to get SignUp to work

--- Milestone 2/src/pages/LoginSignUp.jsx
+++ Milestone 2/src/pages/LoginSignUp.jsx
@@ -1,77 +1,104 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import '../styles/loginsignup.css'
+import "../styles/loginsignup.css";
 
 export function LoginSignUp() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleNameChange = e => {
-    const value = e.target.value
-    setName(value)
-    setErrorMessage('')
-  }
-
-  const handleEmailChange = e => {
-    const value = e.target.value
-    setEmail(value)
-    setErrorMessage('')
-  }
-
-  const handlePhoneChange = e => {
-    const value = e.target.value.replace(/\D/g, '')
-    setPhone(value)
-    setErrorMessage('')
-  }
-
-  const handlePasswordChange = e => {
-    const value = e.target.value
-    setPassword(value)
-    setErrorMessage('')
-  }
-
-  const handleConfirmPasswordChange = e => {
-    const value = e.target.value
-    setConfirmPassword(value)
-    setErrorMessage('')
-  }
-
-  const handleRegister = e => {
-    e.preventDefault();
-  
-    // ...
-    
-    axios.post('mongodb://localhost:27017/mister_pet_db/User', {
-      name,
-      email,
-      phone,
-      password
-    })
-    .then(response => {
-      // Tratar a resposta da sua API de acordo com o que você precisa
-      if (response.data.success) {
-        if (email.endsWith('@admin.com')) {
-          alert('Administrador cadastrado com sucesso!');
-          window.location.href = '/admin';
-        } else {
-          alert('Usuário cadastrado com sucesso!');
-          window.location.href = '/';
-        }
-      }
-    })
-    .catch(error => {
-      // Tratar o erro, exibir uma mensagem de erro para o usuário, etc.
-      console.error(error);
-      setErrorMessage('Ocorreu um erro ao cadastrar o usuário. Por favor, tente novamente.');
-    });
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    setName(value);
+    setErrorMessage("");
   };
-  
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    setErrorMessage("");
+  };
+
+  const handlePhoneChange = (e) => {
+    const value = e.target.value.replace(/\D/g, "");
+    setPhone(value);
+    setErrorMessage("");
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    setErrorMessage("");
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setConfirmPassword(value);
+    setErrorMessage("");
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    // ...
+    const user = { name, email, phone, password };
+    console.log('user: ', user);
+    fetch("/users/:name/email/:email/ps/:password/number/:phoneNumber", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          if (email.endsWith("@admin.com")) {
+            alert("Administrador cadastrado com sucesso!");
+            window.location.href = "/admin";
+          } else {
+            alert("Usuário cadastrado com sucesso!");
+            window.location.href = "/";
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+   /* axios
+      .post("mongodb://localhost:27017/mister_pet_db/User", {
+        name,
+        email,
+        phone,
+        password,
+      })
+      .then((response) => {
+        // Tratar a resposta da sua API de acordo com o que você precisa
+        if (response.data.success) {
+          if (email.endsWith("@admin.com")) {
+            alert("Administrador cadastrado com sucesso!");
+            window.location.href = "/admin";
+          } else {
+            alert("Usuário cadastrado com sucesso!");
+            window.location.href = "/";
+          }
+        }
+      })
+      .catch((error) => {
+        // Tratar o erro, exibir uma mensagem de erro para o usuário, etc.
+        console.error(error);
+        setErrorMessage(
+          "Ocorreu um erro ao cadastrar o usuário. Por favor, tente novamente."
+        );
+      }); */
+  };
 
   return (
     <div className="loginsignup">
@@ -121,12 +148,12 @@ export function LoginSignUp() {
         </button>
         {errorMessage && <p className="loginsignup-error">{errorMessage}</p>}
         <p className="loginsignup-login">
-          Já possui uma conta?{' '}
-          <Link style={{ textDecoration: 'none' }} to="/signin">
+          Já possui uma conta?{" "}
+          <Link style={{ textDecoration: "none" }} to="/signin">
             <span>Entre aqui</span>
           </Link>
         </p>
       </div>
     </div>
-  )
+  );
 }
